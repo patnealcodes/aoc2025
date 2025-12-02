ab08bb1eff42ae2e2f3980882db4b17ebc1a69fc
patnealcodes <hi@patneal.codes>
2025-12-02 12:32:21 -0500

    fin

--- 2/index.test.ts
+++ 2/index.test.ts
@@ -1,14 +1,13 @@
 import { afterEach, beforeAll, describe, expect, test } from "bun:test";
-import { main } from ".";
+import { main } from "./main";
 import { getInput, testConfig } from "../utils";
 
 const day = 2;
 const year = new Date().getFullYear();
 
-// const EXAMPLE_INPUT = `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-// 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-// 824824821-824824827,2121212118-2121212124`;
-const EXAMPLE_INPUT = `95-115`;
+const EXAMPLE_INPUT = `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124`;
 
 const PUZZLE_INPUT = await getInput({ year, day });
 
@@ -18,21 +17,23 @@ describe(`Day ${day}`, async () => {
   });
 
   // test(`Day ${day} First Example`, () => {
-  //   testConfig.log = true;
-
-  //   const { mirrored } = main(EXAMPLE_INPUT);
-  //   expect(mirrored).toBe(1227775554);
+  //   const { doubledSum } = main(EXAMPLE_INPUT);
+  //   expect(doubledSum).toBe(1227775554);
   // });
 
-  test(`Day ${day} Second Example`, () => {
-    testConfig.log = true;
+  // test(`Day ${day} Second Example`, () => {
+  //   testConfig.log = true;
+  //   const { totalSum } = main(EXAMPLE_INPUT);
+  //   expect(totalSum).toBe(4174379265);
+  // });
 
-    const { mirrored, repeated } = main(EXAMPLE_INPUT);
-    expect(mirrored + repeated).toBe(4174379265);
-  });
+  // test(`Day ${day} First Solution`, async () => {
+  //   const { doubledSum } = main(PUZZLE_INPUT);
+  //   expect(doubledSum).toBe(18595663903);
+  // });
 
-  test.skip(`Day ${day} Solution`, async () => {
-    const { mirrored, repeated } = main(PUZZLE_INPUT);
-    expect(mirrored).toBe(18595663903);
+  test(`Day ${day} Second Solution`, async () => {
+    const { totalSum } = main(PUZZLE_INPUT);
+    expect(totalSum).toBe(19058204438);
   });
 });
--- 2/index.ts
+++ 2/index.ts
@@ -8,47 +8,36 @@ function repeatingChunks(id: string, size: number) {
 }
 export const main = (input: string) => {
   const splitInput = input.split(",");
-  const mirroredIDs = [];
+  const doubledIDs = [];
   const repeatedIDs = [];
 
   for (const input of splitInput) {
     const [firstID, lastID] = input.split("-").map((n) => parseInt(n));
-    log(`checking ${input}`);
-    log(`firstID ${firstID}`);
-    log(`lastID ${lastID}`);
 
     if (!firstID || !lastID) break;
 
     for (let i = firstID; i <= lastID; i++) {
       const id = i.toString();
+      let doubled = false;
 
-      // Mirrored
+      // Doubled
       if (id.length % 2 === 0) {
         const half = id.length / 2;
         if (id.slice(0, half) === id.slice(half)) {
-          log(id, "is mirrored");
-          mirroredIDs.push(i);
+          doubledIDs.push(i);
+          doubled = true;
+          log(`${id} is doubled`);
         }
-        // Repeating
-      } else {
-        log(`${id} not mirrored`);
-        // check single digits repeating before resorting to a loop
-        log('id.split("")', id.split(""));
-        log('new Set(id.split("")).size', new Set(id.split("")).size);
-        if (new Set(id.split("")).size === 1) {
-          log(`${new Set(id.split("")).size} repeats every char`);
-          repeatedIDs.push(i);
-        } else {
-          // loop through the different chunk sizes. start at 2 since we checked 1 already
-          for (let j = 2; j < id.length - 1; j++) {
-            log(`checking for ${j} repeating chunks in ${id}`);
-            // If the id can't evenly be chunked to this size, skip it
-            if (id.length % j === 0) {
-              log(`${id} is evenly divisible by ${j}`);
-              if (repeatingChunks(id, j)) {
-                log(`${id} repeats in chunks of ${j}`);
-                repeatedIDs.push(i);
-              }
+      }
+
+      // Repeating
+      if (!doubled) {
+        for (let j = 1; j <= id.length / 2; j++) {
+          if (id.length % j === 0) {
+            if (repeatingChunks(id, j)) {
+              log(`${id} repeats in chunks of ${j}`);
+              repeatedIDs.push(i);
+              log(`${id} is repeated`);
             }
           }
         }
@@ -57,12 +46,21 @@ export const main = (input: string) => {
     log("\n");
   }
 
+  const doubledSum = doubledIDs.reduce((a, b) => a + b, 0);
+  const repeatedSum = repeatedIDs.reduce((a, b) => a + b, 0);
+  const doubledLength = doubledIDs.length;
+  const repeatedLength = repeatedIDs.length;
+
   const answer = {
-    mirrored: mirroredIDs.reduce((a, b) => a + b),
-    repeated: repeatedIDs.reduce((a, b) => a + b),
-    // repeated: 0,
+    doubledLength,
+    doubledSum,
+    repeatedLength,
+    repeatedSum,
+    totalLength: doubledIDs.length + repeatedIDs.length,
+    totalSum: doubledSum + repeatedSum,
   };
 
+  log(`${doubledIDs.length + repeatedIDs.length} invalid codes`);
   log({ answer });
 
   return answer;
