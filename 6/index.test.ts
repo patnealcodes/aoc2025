04cbd7cb8ba4f3fddc5145fc21ad7c5506e34186
patnealcodes <hi@patneal.codes>
2025-12-06 03:21:54 -0500

    cephalopods shouldn't be doing math anyway

--- 6/index.test.ts
+++ 6/index.test.ts
@@ -1,11 +1,19 @@
 import { afterEach, beforeAll, describe, expect, test } from "bun:test";
-import { main } from ".";
+import { main, wtf } from ".";
 import { getInput, testConfig } from "../utils";
 
 const day = 6;
 const year = new Date().getFullYear();
 
-const EXAMPLE_INPUT = `<EXAMPLE_INPUT_GOES_HERE>`;
+const EXAMPLE_INPUT = `123 328  51 64
+ 45 64  387 23
+  6 98  215 314
+*   +   *   +  `;
+const EXAMPLE_INPUT_2 =
+  `123 328  51 64  \n` +
+  ` 45 64  387 23  \n` +
+  `  6 98  215 314 \n` +
+  `*   +   *   +  `;
 
 const PUZZLE_INPUT = await getInput({ year, day });
 
@@ -18,11 +26,30 @@ describe(`Day ${day}`, async () => {
     testConfig.log = true;
 
     const answer = main(EXAMPLE_INPUT);
-    expect(answer).toBe(37);
+    expect(answer).toBe(4277556);
   });
 
-  test.skip(`Day ${day} Solution`, async () => {
+  test(`Day ${day} Solution`, async () => {
     const answer = main(PUZZLE_INPUT);
-    expect(answer).toBe(37);
+    expect(answer).toBe(5227286044585);
+  });
+
+  test(`Day ${day} 2 Example Trying to make sense of it`, () => {
+    testConfig.log = true;
+
+    const { problems, answer } = wtf(EXAMPLE_INPUT_2);
+    expect(problems[0]!.toString()).toBe([1, 24, 356].toString());
+  });
+
+  test(`Day ${day} 2 Example`, () => {
+    testConfig.log = true;
+
+    const { answer } = wtf(EXAMPLE_INPUT_2);
+    expect(answer).toBe(3263827);
+  });
+
+  test(`Day ${day} Solution`, async () => {
+    const { answer } = wtf(PUZZLE_INPUT);
+    expect(answer).toBe(10227753257799);
   });
 });
--- 6/index.ts
+++ 6/index.ts
@@ -1,9 +1,88 @@
 import { log } from "../utils/config";
 
+function cephalopodMath(terms: Array<string>, length: number) {
+  const p = Array.from({ length }, () => "");
+  for (let term of terms) {
+    for (let i = term.length - 1; i >= 0; i--) {
+      p[i]! += term[i];
+    }
+  }
+  return p.map((pr) => parseInt(pr, 10));
+}
+
+export const wtf = (input: string) => {
+  let answer = 0;
+
+  const rows = input.split("\n");
+  let operators = rows.pop();
+  if (!operators) {
+    operators = rows.pop();
+  }
+  const problems = [];
+  let currentOperator = operators![0];
+
+  //* Get column ranges based on operators *//
+  let prev = 0; // store previous slice point
+  for (let i = 1; i <= operators!.length; i++) {
+    const lastIndex = i >= operators!.length;
+    if (/\+|\*/.test(operators![i]!) || lastIndex) {
+      const next = lastIndex ? i : i - 1; // i'm too tired for off-by-one errors right now
+      const pr = [];
+      for (let j = 0; j < rows.length; j++) {
+        pr.push(rows[j]!.slice(prev, next));
+      }
+      const problem = cephalopodMath(pr, next - prev);
+      const thisAns = problem.reduce(
+        (acc, curr) => {
+          if (currentOperator === "*") {
+            return (acc *= curr);
+          } else {
+            return (acc += curr);
+          }
+        },
+        currentOperator === "*" ? 1 : 0,
+      );
+      answer += thisAns;
+      problems.push(problem);
+      prev = i;
+      currentOperator = operators![i];
+    }
+  }
+
+  return {
+    problems,
+    answer,
+  };
+};
+
 export const main = (input: string) => {
-  const splitInput = input.split("\n");
+  let ans = 0;
+  const rows = input
+    .trim()
+    .split("\n")
+    .map((r) => r.trim().split(/\s+/).map(String));
+  const operators = rows.pop();
 
-  log(`input length: ${splitInput.length}`);
+  for (let i = 0; i <= operators!.length - 1; i++) {
+    const op = operators![i];
 
-  return splitInput.length;
+    switch (op) {
+      case "+":
+        const sum = rows
+          .map((row) => row[i])
+          .reduce((acc, curr) => acc + Number(curr), 0);
+        ans += sum;
+        break;
+      case "*":
+        const product = rows
+          .map((row) => row[i])
+          .reduce((acc, curr) => acc * Number(curr), 1);
+        ans += product;
+        break;
+      default:
+        throw new Error(`Unknown operator: ${op}`);
+    }
+  }
+
+  return ans;
 };
